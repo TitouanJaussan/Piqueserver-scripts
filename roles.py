2f5d10245495ceba5193c67ed4e689791d9c997d
TitouanJaussan <tijaussan@outlook.fr>
2024-12-01 19:14:39 +0100

    Alote of changes hope it woks :)

--- roles.py
+++ roles.py
@@ -3,17 +3,19 @@ from piqueserver.commands import command
 import json
 import os
 
+@command("change_role", admin_only=True)
+def change_role(connection, user: str, _role: str):
+    save_role(connection, user, _role)
 
 @command("role")
-def role(connection, role: str):
-    save_role(connection.name, role)
+def role(connection, *args):
+    save_role(connection, connection.name, " ".join(args))
 
 def get_json() -> dict:
     try:
         file = open("roles.json", "r")
         out = json.load(file)
 
-        print(out)
         file.close()
         return out
 
@@ -27,9 +29,11 @@ def load_role(user_name: str) -> str:
     data = get_json()
     return data.get(user_name)
 
-def save_role(user_name: str, role: str) -> None:
+def save_role(connection, user_name: str, role: str) -> None:
     data = get_json()
     data[user_name] = role
+
+    connection.send_chat(f"Role succesfully updated to {role}!")
 
     with open("roles.json", "w") as file:
         json.dump(data, file, indent=4)
@@ -40,10 +44,9 @@ def apply_script(protocol, connection, config):
             role: str = load_role(name)
 
             if role == None:
-                self.send_chat(f"You don't have a role yet !\nGet one with /role <your role>.\nExample: /role 'the Master of Bamboo' -> {name} the Master of bamboo connected !")
-                self.protocol.broadcast_chat(f"{name}, roleless connected !")
+                self.send_chat(f"You don't have a role yet!")
             else:
-                self.protocol.broadcast_chat(f"{name}, {role} connected !")
+                self.protocol.broadcast_chat(f"{name}, {role} connected!")
 
             return connection.on_login(self, name)
 
